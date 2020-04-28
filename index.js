321bacd8f080a66a321801a78bd5956bdfdaecb9
Franklin Tarter <franklintarter@gmail.com>
2020-04-27 22:15:24 -0700

    keyboard commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require("readline");
+const inquirer = require("inquirer");
 
 const Cube = require("./src/cube");
 const render = require("./src/renderer");
@@ -47,9 +47,102 @@ render(cube);
 
 let running = true;
 
-while (running) {
-  let command = readline();
-  if (command === "q") {
-    running = false;
-  }
+// function prompt() {
+//   const questions = [
+//     {
+//       name: "username",
+//       type: "list",
+//       message: "Enter your GitHub username or e-mail address:",
+//       choices: ["Rotate Up (up)", "Rotate Down (down)", "Right", "Left"],
+//       validate: function (value) {
+//         if (value.length) {
+//           return true;
+//         } else {
+//           return "Please enter your username or e-mail address.";
+//         }
+//       },
+//     },
+//   ];
+//   return inquirer.prompt(questions);
+// }
+
+// // while (running) {
+// // }
+// const result = prompt();
+// console.log(result);
+
+var stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+stdin.setRawMode(true);
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+
+// i don't want binary, do you?
+stdin.setEncoding("utf8");
+
+function printKeys() {
+  console.log();
+  console.log("Right  (r)");
+  console.log("Right` (f)");
+  console.log("Up     (e)");
+  console.log("Up     (d)");
+  console.log("Down   (w)");
+  console.log("Down`  (s)");
+  console.log("Left   (q)");
+  console.log("Left`  (a)");
 }
+
+printKeys();
+
+// on any data into stdin
+stdin.on("data", function (key) {
+  // if (key === "")
+
+  switch (key) {
+    case "r":
+      cube.turnRight();
+      break;
+    case "f":
+      cube.turnRight();
+      cube.turnRight();
+      cube.turnRight();
+      break;
+    case "e":
+      cube.turnUp();
+      break;
+    case "d":
+      cube.turnUp();
+      cube.turnUp();
+      cube.turnUp();
+      break;
+    case "w":
+      cube.turnDown();
+      break;
+    case "s":
+      cube.turnDown();
+      cube.turnDown();
+      cube.turnDown();
+      break;
+    case "q":
+      cube.turnLeft();
+      break;
+    case "a":
+      cube.turnLeft();
+      cube.turnLeft();
+      cube.turnLeft();
+      break;
+  }
+
+  // ctrl-c ( end of text )
+  if (key === "\u0003") {
+    process.exit();
+  }
+  // write the key to stdout all normal like
+  render(cube);
+  printKeys();
+
+  // process.stdout.write(key);
+});
